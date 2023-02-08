0c62901a23d551721c781a8000e8add702ee56d8
gulshan25 <g.r.tanny@gmail.com>
2023-02-08 18:29:10 +0600

    React beginners

---
+++ index.js
@@ -0,0 +1,20 @@
+// var works out of the box.
+// const & let works scope in the box.
+
+
+// let using is the best.
+
+
+
+// var name = "Ryan";
+// name = "zen";
+// alert(name);
+
+// const name = "John";
+// alert(name);
+
+// if (true) {
+//     let name = "John";
+//     name = "Smith";
+//     alert(name);
+// }
