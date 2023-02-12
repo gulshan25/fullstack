d321aeccc6ce5a56e8bc06d0ac7776aa8ff8c480
gulshan25 <g.r.tanny@gmail.com>
2023-02-12 15:51:56 +0600

    React beginners

--- index.js
+++ index.js
@@ -1,151 +1,31 @@
-//Creating variables using var//
-
-//var name = "Ryan";
-//name = "John";
-//alert(name);
-
-// if (true) {
-//     var name = "John";
-//     //alert(name);
+// var name = "Everest";
+// var height = "8848";
+// var output = function() {
+//     console.log(`Mt. ${this.name} is ${this.height} meter tall.`);
 // }
 
-// alert(name);
-
-//Creating variables using var//
-
-//Creating variables using const//
-
-// const name = "Smith";
-// alert(name);
-
-//Creating variables using const//
-
-//Creating variables using let//
-
-// if (true) {
-//     const name = "John";
-//     //     name = "Smith";
-//     alert(name);
-// }
-
-//Creating variables using let//
-
-//Creating template strings//
-
-// let fname = "John";
-// let lname = "Smith";
-// let age = prompt("Guess Ryan's age...");
-
-//old way concet//
-// let result = fname + ' ' + lname + ' is' + ' ' + age + ' years old';
-// alert(result);
-
-//using template strings
-//this method can be using in API//
-
-// let result = `${fname} ${lname} is ${age} years old`;
-// alert(result);
-
-//Creating template strings//
-
-//Default parameters//
-
-// function welcome(user, message) {
-//     alert(`${user}, ${message}`);
-// }
-// welcome('Gulshan', 'Good Night');
-
-//If don't given info
-
-// function welcome(user, message) {
-//     alert(`Hello ${user}, ${message}`);
-// }
-// welcome('ryan', 'Good morning');
-
-// function welcome(user = "mystery person", message = "Good Day") {
-//     alert(`Hello ${user}, ${message}`);
-// }
-// welcome();
-
-//Default parameters//
-
-//arrow functions//
-
-// function greeting(message) {
-//     return alert(`${message} everyone!`);
-// }
-// greeting("good day");
-
-// function greeting(message = "good day") {
-//     return alert(`${message} everyone!`);
-// }
-// greeting();
-
-//ES6 method
-
-// let greeting = message => alert(`${message} Everyone!`);
-// greeting('hello');
-
-// let greeting = (user, message) => alert(`Hello ${user}, ${message}`);
-// greeting('ryan', 'Good night');
-
-// let greeting = (user = 'mystery person', message = 'good day') => alert(`Hello ${user}, ${message}`);
-// greeting();
+// var adventureClimbing = { name, height, output };
+// adventureClimbing.output();
 
 
-//arrow functions//
+//ES6//
 
-//arrow functions & this kayword//
+// var adventureClimbing = {
+//     name: "Everest",
+//     height: "8848",
+//     output: function() {
+//         console.log(`Mt. ${this.name} is ${this.height} meter tall.`);
+//     }
+// };
+// adventureClimbing.output();
 
-let nepal = {
-    //add property
-    mountains: ['Everest', ' Fish Tail', ' Annapurna'],
-    //add method
-    printWithDash: function() {
-
-        setTimeout(() => console.log(this.mountains.join("-")), 3000)
+var adventureClimbing = {
+    name: "Everest",
+    height: "8848",
+    output() {
+        console.log(`Mt. ${this.name} is ${this.height} meter tall.`);
     }
 };
+adventureClimbing.output();
 
-nepal.printWithDash();
-
-//arrow functions & this kayword//
-
-//destructuring object//
-
-let thingsToDo = {
-    morning: "exercise",
-    afternoon: "work",
-    evening: "code",
-    night: ["sleep", "Dream"],
-}
-let { morning, afternoon } = thingsToDo;
-morning = "run";
-console.log(morning, '-', afternoon);
-
-let uniStudent = ({ name, university }) => {
-    //let { name, university } = student;
-    console.log(`${name} from ${university}`);
-
-};
-
-uniStudent({
-    name: "ryan",
-    university: "America"
-});
-//destructuring object//
-
-//destructuring array//
-
-let [, , firstMountain] = ['Everest', 'Fish Tail', 'Annapurna']
-console.log(firstMountain);
-//destructuring array//
-
-//restructuring//
-
-var name = 'Everest';
-var height = 8848;
-
-var adventureClimbing = {}
-
-//restructuring//
+//ES6//
